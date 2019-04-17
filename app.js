6934631e4aee39677afed736e587819d375f058a
kk5353 <kk5353@aliyun.com>
2019-04-17 15:37:35 +0800

    孔磊修正，含安第一次推送信息

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const express = require('express')()
-var server = express().listen(8080);
+const express = require('express');
+var server = express();
+server.listen(8080);
 console.log('http://127.0.0.1:8080'  );
 server.use('/',express.static('static'));
 
