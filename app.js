cdb0ff6bd53f81a772f4da76e12738b8c052116e
kk5353 <kk5353@aliyun.com>
2019-04-17 13:52:24 +0800

    减少依赖版本d

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const multer = require('multer');
-const cookieParser = require('cookie-parser');
-const cookieSession = require('cookie-session');
-const config = require('./config/config');
-const path = require('path')
-//API接口服务
-var server = express();
-server.listen(8080);
+const express = require('express')()
+var server = express().listen(8080);
 console.log('http://127.0.0.1:8080'  );
 server.use('/',express.static('static'));
 
