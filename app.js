14643d0784f70aed2680dea79bc7b8976d029449
AmberAAA <anborong@outlook.com>
2019-04-17 16:25:23 +0800

    1. add gitignore 2. 配置路由

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const history = require('connect-history-api-fallback');
 var server = express();
 server.listen(8080);
 console.log('http://127.0.0.1:8080'  );
+server.use(history({
+    verbose: true,
+    index: '/'
+}));
 server.use('/',express.static('static'));
 
 
